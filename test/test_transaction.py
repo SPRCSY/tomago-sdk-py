9c31e937f9686361d85e6255ff85fedf481a0fe6
SPRCSY <36022626+SPRCSY@users.noreply.github.com>
2018-07-22 17:40:45 +0800

    Update test_transaction.py

--- test/test_transaction.py
+++ test/test_transaction.py
@@ -1,14 +1,10 @@
 """
-Copyright ArxanFintech Technology Ltd. 2018 All Rights Reserved.
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-                 http://www.apache.org/licenses/LICENSE-2.0
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+版权所有ArxanFintech Technology Ltd. 2018保留所有权利。
+根据Apache许可证2.0版（“许可证”）获得许可; 除非符合许可，否则您不得使用此文件。 您可以在以下位置获取许可证副本
+                 http://www.apache.org/licenses/LICENSE-2.0
+除非适用法律要求或书面同意，否则根据许可证分发的软件按“原样”分发，不附带任何明示或暗示的担保或条件。
+有关管理许可下的权限和限制的特定语言，请参阅许可证。
+
 """
 
 import unittest
